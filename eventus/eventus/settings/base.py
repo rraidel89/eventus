9aa49356a7a100ae05295117ad3b646acf04d8d1
Francisco Mora <francisco.mora.sanchez@gmail.com>
2014-10-24 09:59:43 +0200

    Commit inicial

---
+++ eventus/eventus/settings/base.py
@@ -0,0 +1,47 @@
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+SECRET_KEY = 'e9hod6u+i%*6(5g+(@f=b%my7n%c*+8xqc#96u1j#(kf3ppxgt'
+
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = []
+
+DJANGO_APPS  = (
+        'django.contrib.admin',
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'django.contrib.messages',
+        'django.contrib.staticfiles',
+    )
+
+LOCAL_APPS = (
+        'apps.events',
+    )
+
+THIRD_PARTY_APPS = (
+    )
+
+INSTALLED_APPS = DJANGO_APPS + LOCAL_APPS + THIRD_PARTY_APPS
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'eventus.urls'
+WSGI_APPLICATION = 'eventus.wsgi.application'
+
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
